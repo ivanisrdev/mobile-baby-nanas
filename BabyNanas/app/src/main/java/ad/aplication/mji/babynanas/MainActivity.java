33ddaf1ac26f695048b66374965e191ceb9e29e2
Spiderbezno <j2agm@hotmail.com>
2017-02-18 19:01:37 +0100

    Add tabLayout, with fake content

--- BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MainActivity.java
+++ BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MainActivity.java
@@ -1,13 +1,25 @@
 package ad.aplication.mji.babynanas;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +48,13 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        MusicTypePagerAdapter adapter = new MusicTypePagerAdapter(getSupportFragmentManager(),
+                this.getApplicationContext());
+        ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
@@ -52,5 +71,61 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class MusicTypeFragment extends Fragment {
+        private static final String TAB_POSITION = "tab_position";
+
+        public MusicTypeFragment() {
+
+        }
+
+        public static MusicTypeFragment newInstance(int tabPosition) {
+            MusicTypeFragment fragment = new MusicTypeFragment();
+            Bundle args = new Bundle();
+            args.putInt(TAB_POSITION, tabPosition);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Bundle args = getArguments();
+            int tabPosition = args.getInt(TAB_POSITION);
+            TextView tv = new TextView(getActivity());
+            tv.setGravity(Gravity.CENTER);
+            tv.setText("Text in Tab #" + tabPosition);
+            return tv;
+        }
+    }
+
+    class MusicTypePagerAdapter extends FragmentStatePagerAdapter {
+
+        final int PAGE_COUNT = 3;
+        private String tabTitles[] = new String[] { getString(R.string.nanas),
+                getString(R.string.relax), getString(R.string.classical) };
+        private Context context;
+
+        public MusicTypePagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return MusicTypeFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            // Generate title based on item position
+            return tabTitles[position];
+        }
     }
 }
