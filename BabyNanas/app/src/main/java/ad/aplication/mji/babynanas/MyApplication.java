f0ddcb337f2ca528935e9d0ed6c70728c806a064
Ivan <ivan.sanchez3@gmail.com>
2017-07-19 00:26:37 +0200

    add music relax and change quality mp3

--- BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MyApplication.java
+++ BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MyApplication.java
@@ -61,6 +61,8 @@ public class MyApplication extends Application {
 
       realm.beginTransaction();
       // Add a music type Nana
+      Music musicNana6 = new Music(6L,"A dormir","Nana",0,"baby12");
+      realm.copyToRealmOrUpdate(musicNana6);
       Music musicNana1 = new Music(1L,"Angel feliz","Nana",0,"baby5");
       realm.copyToRealmOrUpdate(musicNana1);
       Music musicNana2 = new Music(2L,"Arrorro","Nana",0,"baby6");
@@ -69,27 +71,37 @@ public class MyApplication extends Application {
       realm.copyToRealmOrUpdate(musicNana3);
       Music musicNana4 = new Music(4L,"Estrellita donde estas","Nana",0,"baby8");
       realm.copyToRealmOrUpdate(musicNana4);
-      Music musicNana5 = new Music(5L,"Mama naturaleza","Nana",0,"baby3");
+      Music musicNana5 = new Music(5L,"Ea la nana","Nana",0,"baby3");
       realm.copyToRealmOrUpdate(musicNana5);
-      Music musicNana6 = new Music(6L,"Muñequita linda","Nana",0,"baby12");
-      realm.copyToRealmOrUpdate(musicNana6);
 
       // Add a music type Relax
       Music musicRelax1 = new Music(7L,"Agua","Relax",0,"agua");
       realm.copyToRealmOrUpdate(musicRelax1);
+      Music musicRelax12 = new Music(23L,"Arpa Celta","Relax",0,"arpa1");
+      realm.copyToRealmOrUpdate(musicRelax12);
+      Music musicRelax14 = new Music(25L,"Arpa Relax","Relax",0,"arpa2");
+      realm.copyToRealmOrUpdate(musicRelax14);
+      Music musicRelax10 = new Music(20L,"Arroyo de Agua","Relax",0,"agua2");
+      realm.copyToRealmOrUpdate(musicRelax10);
       Music musicRelax2 = new Music(8L,"Corazon","Relax",0,"corazon");
       realm.copyToRealmOrUpdate(musicRelax2);
+      Music musicRelax11 = new Music(21L,"Flauta Japonesa","Relax",0,"flauta");
+      realm.copyToRealmOrUpdate(musicRelax11);
       Music musicRelax3 = new Music(9L,"Fuego","Relax",0,"fuego");
       realm.copyToRealmOrUpdate(musicRelax3);
       Music musicRelax4 = new Music(10L,"Lluvia","Relax",0,"lluvia");
       realm.copyToRealmOrUpdate(musicRelax4);
       Music musicRelax5 = new Music(11L,"Mar","Relax",0,"mar");
       realm.copyToRealmOrUpdate(musicRelax5);
+      Music musicRelax9 = new Music(22L,"Notas Musicales","Relax",0,"notas");
+      realm.copyToRealmOrUpdate(musicRelax9);
+      Music musicRelax13 = new Music(24L,"Nothing Else Matters Arpa","Relax",0,"nothing");
+      realm.copyToRealmOrUpdate(musicRelax13);
       Music musicRelax6 = new Music(12L,"Pajaros","Relax",0,"pajaros");
       realm.copyToRealmOrUpdate(musicRelax6);
       Music musicRelax7 = new Music(13L,"Viento","Relax",0,"viento");
       realm.copyToRealmOrUpdate(musicRelax7);
-      Music musicRelax8 = new Music(14L,"Vientre","Relax",0,"baby4");
+      Music musicRelax8 = new Music(14L,"Vientre","Relax",0,"vientre");
       realm.copyToRealmOrUpdate(musicRelax8);
 
       // Add a music type Classical
