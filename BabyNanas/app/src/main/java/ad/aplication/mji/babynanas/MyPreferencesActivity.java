9a191a10872c8128b7aa6e2c8017fa4a738d0959
Marc Riu <marc.riu@gmail.com>
2018-01-18 03:05:34 +0100

    no compila implementation files('libs/YouTubeAndroidPlayerApi.jar') i la part de idioma

--- BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MyPreferencesActivity.java
+++ BabyNanas/app/src/main/java/ad/aplication/mji/babynanas/MyPreferencesActivity.java
@@ -1,8 +1,18 @@
 package ad.aplication.mji.babynanas;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
 
 public class MyPreferencesActivity extends PreferenceActivity {
 
@@ -19,7 +29,83 @@ public class MyPreferencesActivity extends PreferenceActivity {
     {
       super.onCreate(savedInstanceState);
       addPreferencesFromResource(R.xml.preferences);
+
+      //aixo cal?
+      final ListPreference listPreference = (ListPreference) findPreference("language");
+      setListPreferenceData(listPreference);
+
     }
   }
 
+
+  public static String[] getAppSupportedLaungages(Context context, String[] langs) {
+
+    Configuration conf = context.getResources().getConfiguration();
+
+    String[] displayNames = new String[langs.length];
+    displayNames[0] = "Auto";
+    int size = langs.length;
+    for (int i=1; i < size; i++) {
+      conf.locale = new Locale(langs[i]);
+      displayNames[i] = ucFirst(conf.locale.getDisplayName());
+    }
+
+    return displayNames;
+  }
+
+
+  public static Map<String, String> getAppSupportedLaungages(Context context, String appDefaultLang) {
+    Map<String, String> listAppLocales = new LinkedHashMap<>();
+
+    listAppLocales.put("default", "Auto");
+
+    DisplayMetrics metrics = new DisplayMetrics();
+    Resources res = context.getResources();
+    Configuration conf = res.getConfiguration();
+    String[] listLocates = res.getAssets().getLocales();
+
+    for (String locate : listLocates) {
+      Log.i("Member name: ",locate);
+    }
+
+
+    for (String locate : listLocates) {
+      conf.locale = new Locale(locate);
+      Resources res1 = new Resources(context.getAssets(), metrics, conf);
+      String s1 = res1.getString(R.string.configuration);
+      String value = String.format(Locale.getDefault(),"%s (%s)",
+              ucFirst(conf.locale.getDisplayName()),
+              conf.locale.getLanguage()
+      );
+
+      conf.locale = new Locale("");
+      Resources res2 = new Resources(context.getAssets(), metrics, conf);
+      String s2 = res2.getString(R.string.configuration);
+
+      if (!s1.equals(s2)) {
+        //Log.d("DIFFERENT LOCALE", i + ": "+ s1+" "+s2 +" "+ loc[i]);
+        listAppLocales.put(locate, value);
+      } else if (locate.equals(appDefaultLang)) {
+        listAppLocales.put(locate, value);
+      }
+    }
+    return listAppLocales;
+  }
+
+  public static String ucFirst(String subject) {
+    if (!subject.isEmpty()) {
+      return Character.toUpperCase(subject.charAt(0)) + subject.substring(1);
+    } else {
+      return null;
+    }
+  }
+  private static void setListPreferenceData(ListPreference lp) {
+
+    String[] entryValues = MyApplication.getInstance().getApplicationContext().getResources().getStringArray(R.array.pref_language_list_values);
+    CharSequence[] entries = getAppSupportedLaungages(MyApplication.getInstance().getApplicationContext(), entryValues);
+
+    lp.setEntries(entries);
+    lp.setEntryValues(entryValues);
+    lp.setDefaultValue("default");
+  }
 }
